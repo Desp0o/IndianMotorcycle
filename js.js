c9c432fa8779c452f3edb93b9df30a1aa20f66f5
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2022-06-19 01:23:24 +0400

    update blured page and burger menu

--- js.js
+++ js.js
@@ -1,11 +1,18 @@
 document.getElementById("menu").addEventListener('click',(e)=>{
     e.preventDefault();
-    document.getElementById("burger").style.right ="0px";
-    document.querySelector('body').style.overflow ="hidden";    
+    document.getElementById("burger").style.right = "0px";
+    document.querySelector('body').style.overflow = "hidden";  
+    document.getElementById('blur').style.display = "block";  
 })
 
 document.getElementById('close_btn').addEventListener('click', () => {
-    document.getElementById("burger").style.right ="-300px";
-    document.querySelector('body').style.overflow ="scroll";  
-    document.querySelector('body').style.overflowX ="hidden"; 
+    document.getElementById("burger").style.right = "-300px";
+    document.querySelector('body').style.overflow = "scroll";  
+    document.querySelector('body').style.overflowX = "hidden"; 
+    document.getElementById('blur').style.display = "none";
+})
+
+document.getElementById('blur').addEventListener('click', () => {
+    document.getElementById("burger").style.right = "-300px";
+    document.getElementById('blur').style.display = "none";
 })
