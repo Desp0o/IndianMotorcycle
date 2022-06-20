d4b823bad77a37b78309390ca86eccd7cd472568
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2022-06-21 00:59:37 +0400

    fixed scroll event

--- scroll.js
+++ scroll.js
@@ -1,3 +1,5 @@
+
+
 const reveals = document.querySelectorAll('.content');
 
     window.addEventListener('load', () => {
@@ -16,13 +18,27 @@ window.addEventListener('scroll', () => {
         const windowHeight = window.innerHeight;
         const revealTop = reveals[i].getBoundingClientRect().top;
         const revealpoint = 350;
-
+        const revealEndPoint = 680;
+        
             if(revealTop < windowHeight - revealpoint){
                 reveals[i].classList.add('active');
             }else{
                 reveals[i].classList.remove('active');
             }  
+
+            if(revealTop < windowHeight - revealEndPoint){
+                reveals[i].classList.remove('active');
+            }
+
+            
+            // ნახავს სქროლის პოზიციას
+            localStorage.scrollX = window.scrollX;
+            localStorage.scrollY = window.scrollY;
     }
 }
 );
+
+window.addEventListener('load',function () {
+    window.scrollTo(localStorage.scrollX || 0, localStorage.scrollY || 0);
+})
 
